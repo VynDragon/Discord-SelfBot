2ba101b0a446ca5231a66f7547c86080698c5fda
VynDragon <Vdragon.b@gmail.com>
2017-06-30 16:32:46 +0200

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -4,6 +4,8 @@ import asyncio
 import argparse
 import traceback
 import subprocess
+import sys
+from io import StringIO
 
 
 client = discord.Client()
@@ -15,23 +17,49 @@ async def on_ready():
 	print(client.user.id)
 	print('------')
     
+stack = ""
 @client.event
 async def on_message(message):
+	global stack
 	print('msg by ' + message.author.name + ": " + message.content)
 	if (not message.author.name == "Vdragon"):
 		return
 	if message.content.startswith('::run'):
+		if len(message.content[6:]) < 1:
+			old_stdout = sys.stdout
+			try:
+				redirected_output = sys.stdout = StringIO()
+				exec(stack)
+				await client.send_message(message.channel, '```' + str(redirected_output.getvalue()) + '```')
+			except Exception as e:
+				await client.send_message(message.channel, '```' + str(traceback.format_exc()).replace("```", "\`\`\`") + '```')
+			finally:
+				sys.stdout = old_stdout
+		else:
+			try:
+				await client.send_message(message.channel, '```' + str(eval(message.content[6:])) + '```')
+			except Exception as e:
+				await client.send_message(message.channel, '```' + str(traceback.format_exc()).replace("```", "\`\`\`") + '```')
+	if message.content.startswith('::stack'):
+		stack +=  "\n" + message.content[8:]
+		
+	if message.content.startswith('::unstack'):
 		try:
-			await client.send_message(message.channel, '```' + str(eval(message.content[6:])) + '```')
+			stack = stack[:stack.rfind('\n')]
 		except Exception as e:
 			await client.send_message(message.channel, '```' + str(traceback.format_exc()).replace("```", "\`\`\`") + '```')
-	if message.content.startswith('::bash'):
+	if message.content.startswith('::cleanstack'):
+		stack = ""
+		await client.send_message(message.channel, '```' + str("stack cleaned") + '```')
+	if message.content.startswith('::pstack'):
+		await client.send_message(message.channel, '```' + str(stack) + '```')
+#	if message.content.startswith('::bash'):
 
 
 
 bashing = None
 
-async def check_bash():
+#async def check_bash():
 	
 	
 
